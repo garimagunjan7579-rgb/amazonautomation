386a37fe4fa68f8dec1cbd30e7ecc4571b5f9eb8
Garima Gunjan <garimagunjan7579@gmail.com>
2026-03-04 19:07:47 +0530

    Driver setup completed

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,39 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class DriverFactory {
+
+    public static void initDriver(String browser) {
+
+        WebDriver driver = null;
+
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        DriverManager.setDriver(driver);
+    }
+}
